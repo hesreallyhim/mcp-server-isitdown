fab8ee38cd709d09c07d85652489679be6630a3d
Really Him <hesereallyhim@proton.me>
2025-02-26 20:37:43 -0500

    fix: fix potential indexing error and README typo

--- isitdown.py
+++ isitdown.py
@@ -1,24 +1,21 @@
 import asyncio
-from typing import TYPE_CHECKING
 
-TYPE_CHECKING = True
-
-from dotenv.cli import get
 import requests
-from bs4 import  BeautifulSoup as bs
-from bs4._typing import Tag
+from bs4 import BeautifulSoup as bs
+from bs4.element import Tag
 from mcp.server.fastmcp import FastMCP
 
-mcp = FastMCP("isitdown")
+mcp = FastMCP("isitdown", "Help the user determine if a website is down or not.")
 
 ISITDOWN_BASE_URL = "https://www.isitdownrightnow.com/check.php?domain="
-USER_AGENT = "isitdown-app/0.0.1"
+USER_AGENT = "mcp-isitdown-server/0.0.2"
+
 
 def get_last_down(last_down_row: Tag) -> str:
     """
     Extract the last down time from the HTML row.
     Args:
-        last_down_row (str): The HTML row containing the last checked time.
+        last_down_row (bs4.Tag): The HTML row containing the last checked time.
     Returns:
         str: The last time the server found the website to be down.
     """
@@ -26,7 +23,8 @@ def get_last_down(last_down_row: Tag) -> str:
     if last_down_time is None:
         return "Last down time not found."
     else:
-        return f"Last down time is {last_down_time.text.strip()}"
+        return f"Last down time is: {last_down_time.text.strip()}"
+
 
 @mcp.tool()
 async def get_website_status(root_domain: str) -> str:
@@ -39,7 +37,8 @@ async def get_website_status(root_domain: str) -> str:
     Returns:
         str: A message indicating whether the website is up or down, or if the status could not be determined.
     """
-    
+
+    last_down_time = "Could not determine information about the last down time."
     try:
         response = requests.get(
             f"{ISITDOWN_BASE_URL}{root_domain}", headers={"User-Agent": USER_AGENT}
@@ -51,8 +50,8 @@ async def get_website_status(root_domain: str) -> str:
     is_up = soup.find("span", class_="upicon")
     is_down = soup.find("span", class_="downicon")
     tabletrsimple_divs = soup.find_all("div", class_="tabletrsimple")
-    if len(tabletrsimple_divs) >= 1:
-        last_down_row = tabletrsimple_divs[1] # NOTE: Brittle.
+    if len(tabletrsimple_divs) >= 2:
+        last_down_row = tabletrsimple_divs[1]  # NOTE: Brittle.
         if isinstance(last_down_row, Tag):
             last_down_time = get_last_down(last_down_row)
     if is_down:
@@ -61,6 +60,7 @@ async def get_website_status(root_domain: str) -> str:
         return f"The website is up. {last_down_time}."
     else:
         return "Could not determine the status of the website."
+
 
 if __name__ == "__main__":
     asyncio.run(mcp.run_stdio_async())
